9fc4240dda3d285150d803ebc088fab11851c362
Jeff Gilbert <jeffgilbert@example.com>
2025-10-13 14:22:40 -0500

    Added Autopatch remediation

---
+++ Autopatch Remediation/Autopatch_detection.ps1
@@ -0,0 +1,90 @@
+﻿<#
+.SYNOPSIS
+
+.DESCRIPTION
+    
+.PARAMETER     
+
+.EXAMPLE
+
+.NOTES
+
+#>
+
+try {
+    $i = 0
+    
+    $WUkeys = @(
+        "DoNotConnectToWindowsUpdateInternetLocations",
+        "DisableWindowsUpdateAccess"
+    )
+    ForEach ($key in $WUkeys) {
+        If (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate") {
+            if((Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate).PSObject.Properties.Name -contains $key) {
+                $i++
+            } Else{Write-Output "Key not found: $key"}
+        }
+    }
+
+    $AUkeys = @(
+        "NoAutoUpdate"
+    )
+    ForEach ($key in $AUkeys) {
+        If (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU") {
+            if((Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU).PSObject.Properties.Name -contains $key) {
+            $i++
+            } Else{ Write-Output "Key not found: $key" }
+        } 
+
+    }
+    
+    if ( $i -gt 0 ) {
+        Write-Output "Issues detected."
+        Exit 1 # Detection: non-compliant
+    } else {
+        Write-Output "No issues detected."
+        Exit 0 # Detection: compliant
+    }
+    
+} catch {
+    Write-Output "Error occurred: $_"
+}
+
+
+
+<#
+
+
+
+
+
+    $regKeys = @(
+        "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate",
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "DoNotConnectToWindowsUpdateInternetLocations"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "DisableWindowsUpdateAccess"
+
+
+
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name ""
+    )
+    $r = 0
+    ForEach ($regKey in $regKeys) {
+        if (Test-Path $regKey) {
+            $r++
+            Write-Output "Registry key exists: $regKey"
+        }
+    }
+    
+    if( $r -gt 0 ) {
+        Write-Output "Issues detected."
+        Exit 1 # Detection: non-compliant
+    } else {
+        Write-Output "No issues detected."
+        Exit 0 # Detection: compliant
+    }
+}
+
+
+Exit
+
+#>
---
+++ Autopatch Remediation/Autopatch_remediation.ps1
@@ -0,0 +1,85 @@
+﻿<#
+.SYNOPSIS
+
+.DESCRIPTION
+    
+.PARAMETER     
+
+.EXAMPLE
+
+.NOTES
+
+#>
+
+#-------------------------------------------------------- Functions ---------------------------------------------------------
+
+
+#------------------------------------------------------- Begin Script -------------------------------------------------------
+# Logging optional for remediations because the script output is displayed in the Intune portal's device status for your remediation.
+# Logging enabled in this example where Intune can grab it if needed.
+$logDir = "$($env:ProgramData)\Microsoft\IntuneManagementExtension\Logs" # This path is pulled when Intune collects diagnostics.
+$logFile = "$($logDir)\autopatch_remediation.log" # Rename this log file.
+#Start-Transcript $logFile
+
+try {
+    $WUkeys = @(
+        "DoNotConnectToWindowsUpdateInternetLocations",
+        "DisableWindowsUpdateAccess"
+    )
+    ForEach ($key in $WUkeys) {
+        If (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate") {
+            if((Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate).PSObject.Properties.Name -contains $key) {
+                    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name $key -ErrorAction SilentlyContinue
+                    Write-Output "Removed: $key"
+                } Else{Write-Output "Key not found: $key"}
+        } Else{ Write-Output "Path not found: HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\$key" }
+    }
+
+    $AUkeys = @(
+        "NoAutoUpdate"
+    )
+    ForEach ($key in $AUkeys) {
+        If (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU") {
+            If ((Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU).PSObject.Properties.Name -contains $key) {
+                Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name $key -ErrorAction SilentlyContinue
+                Write-Output "Removed: $key"    
+                } Else{ Write-Output "Key not found: $key" }
+            } Else { Write-Output "Path not found: HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\$key" } 
+    }
+
+Write-Output "Remediation completed."
+#Stop-Transcript
+Exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} catch {
+    Write-Error "An error occurred: $_"
+}
+
+Stop-Transcript 
+Exit
